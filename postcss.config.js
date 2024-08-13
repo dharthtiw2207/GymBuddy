4f41fd0115f165ba0e026fb253a1e029ec6e2369
DharthTiw2207 <siddharthatiwari1705@gmail.com>
2024-08-13 22:55:37 +0530

    first commit

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+
+// module.exports = {
+//   plugins: {
+//     'autoprefixer': {},
+//     'postcss-nested': {},
+//     // Add other plugins here
+//   },
+// };
---
+++ src/components/testimonials/Image.jsx
@@ -0,0 +1,8 @@
+function Image() {
+  return (
+    // <div className="absolute bottom-0 z-20 hidden h-[450px] w-2/5 rounded-tr-[50%] bg-[url('./images/testimonials/person.webp')] bg-cover bg-top xl:block"></div>
+    <div className="absolute bottom-0 z-20 hidden h-[450px] w-2/5 rounded-tr-[50%] bg-[url('./images/testimonials/person.png')] bg-cover bg-top xl:block"></div>
+  );
+}
+
+export default Image;
