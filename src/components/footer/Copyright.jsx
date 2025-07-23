05b6a07a9908de58465723a31028f7d1d71517eb
Siddhartha Tiwari <128299952+dharthtiw2207@users.noreply.github.com>
2025-07-23 17:21:38 +0530

    Update Copyright.jsx

--- src/components/footer/Copyright.jsx
+++ src/components/footer/Copyright.jsx
@@ -6,16 +6,6 @@ function Copyright() {
       <p className=" ">
         Privacy Policy | &copy; <span>{new Date().getFullYear()}</span> Gymat
       </p>
-      <p>
-        Designed by{" "}
-        <Link
-          to="https://www.radiustheme.com/"
-          target="_blank"
-          className="focus text-red"
-        >
-          RadiusTheme
-        </Link>
-      </p>
     </div>
   );
 }
